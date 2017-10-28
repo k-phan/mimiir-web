618c8cb9410c1890824cf1d7009daa5f92271b0b
Khai Phan <kphan95@bu.edu>
2017-10-28 15:31:37 -0400

    This fix should be itA

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd /bin/mimiir/mimiir-web/
+cd /bin/mimiir/
 npm install 
 
 pm2 describe mimiir-web > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -ne 0 ]; then
-	pm2 start /bin/mimiir/mimiir-web/bin/www
+	pm2 start /bin/mimiir/bin/www
 else
 	pm2 restart www
 fi;
